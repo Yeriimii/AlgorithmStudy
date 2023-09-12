c505515d5f7017b0b6349768b5baa2a24820629f
Yeriimii <seojs0511@naver.com>
2023-09-12 19:31:30 +0900

    중복 순열 구하기(DFS)

---
+++ 중복순열_구하기(DFS).py
@@ -0,0 +1,24 @@
+import sys
+
+
+def DFS(L):
+    global cnt
+
+    if L == m:  # 중단점
+        for j in range(m):
+            print(res[j], end=' ')  # res 출력
+        print()
+        cnt += 1
+    else:
+        for i in range(1, n + 1):
+            res[L] = i
+            DFS(L+1)
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    res = [0] * m
+    cnt = 0
+    DFS(0)
+    print(cnt)
+
