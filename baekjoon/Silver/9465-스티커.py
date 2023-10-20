265d3c7ece46e2af93ddfc9e89159a397f035926
Yeriimii <seojs0511@naver.com>
2023-10-20 21:50:38 +0900

    해결한 문제

---
+++ baekjoon/Silver/9465-스티커.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    T = int(input())
+    for _ in range(T):
+        n = int(input())
+        dy = [[] for _ in range(n)]
+        board = [list(map(int, input().split())) for _ in range(2)]
+        if n > 1:
+            board[0][1] += board[1][0]
+            board[1][1] += board[0][0]
+        for i in range(2, n):
+            board[0][i] += max(board[1][i-1], board[1][i-2])
+            board[1][i] += max(board[0][i-1], board[0][i-2])
+
+        print(max(board[0][n-1], board[1][n-1]))
