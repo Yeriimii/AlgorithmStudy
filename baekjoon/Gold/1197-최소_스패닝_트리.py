9d63b8a44c3454d5a3d3137bf51e878218a67dc6
Yeriimii <seojs0511@naver.com>
2023-10-30 14:08:46 +0900

    해결한 문제(231030)

---
+++ baekjoon/Gold/1197-최소_스패닝_트리.py
@@ -0,0 +1,35 @@
+import sys
+import heapq
+
+'''Prim 알고리즘: 전체 노드들을 연결할 때 사용한다. (간선이 많을 때)
+1. 임의의 정점을 선택
+2. 해당 정점에서 갈 수 있는 간선을 minheap에 넣는다.
+3. 최소값을 뽑아 해당 정점을 방문 안했다면 선택한다.
+'''
+input = sys.stdin.readline
+
+V, E = map(int, input().split())
+visited = [False] * (V + 1)  # 방문 여부 확인
+Elist = [[] for _ in range(V + 1)]  # 간선 저장
+# 1번 노드는 자기 자신으로 가는 거리가 0 임을 우선순위 큐에 삽입
+heap = [[0, 1]]
+for _ in range(E):
+    s, e, w = map(int, input().split())
+    Elist[s].append([w, e])  # s -> e 연결(가중치 w)
+    Elist[e].append([w, s])  # e -> s 연결(가중치 w)
+
+answer = 0
+cnt = 0
+while heap:
+    if cnt == V:  # 중단점: 모든 노드에 방문한 횟수 == 모든 노드 개수
+        break
+    w, s = heapq.heappop(heap)  # 가중치, 현재 노드
+    if not visited[s]:  # 각 노드당 한 번씩만 방문
+        visited[s] = True
+        answer += w  # 연결 거리 누적 합
+        cnt += 1  # 노드 방문 추가
+        # 현재 노드와 연결된 노드들을 우선순위 큐에 삽입
+        for i in Elist[s]:
+            heapq.heappush(heap, i)
+
+print(answer)
