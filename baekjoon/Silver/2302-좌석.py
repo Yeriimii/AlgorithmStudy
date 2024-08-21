68aefcb2ebebecf8d5bc3acbf6904213d48c7096
Yeriimii <seojs0511@naver.com>
2024-08-22 00:05:17 +0900

    해결한 문제 (20240821)
    - 2302

---
+++ baekjoon/Silver/2302-좌석.py
@@ -0,0 +1,15 @@
+n = int(input())
+m = int(input())
+reserved = [False for _ in range(n + 1)]
+for _ in range(m):
+    reserved[int(input())] = True
+
+dp = [0 for _ in range(n + 1)]
+dp[0] = 1
+
+for i in range(1, n + 1):
+    if reserved[i] or reserved[i - 1]:
+        dp[i] = dp[i - 1]
+    else:
+        dp[i] = dp[i - 1] + dp[i - 2]
+print(dp[-1])
